8cd0ca6fa3e0767c912c2b240b2316c3ee93ed50
Mario Syti <msyti@outlook.com>
2021-06-01 21:43:16 +0300

    03036

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
 
 @Component({
   selector: 'app-card',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input() title = '';
+  @Input() imageUrl = '';
+  @Input() content = '';
+  @Input() username = '';
+  
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
