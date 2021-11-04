b8594bec087e44dc3b5b77eefb7f5ed11b28c03e
Mario Syti <msyti@outlook.com>
2021-11-05 01:42:49 +0200

    fixed the extra card showing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,30 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  posts=[
+  posts = [
     {
       title: 'Neat Tree',
-      imageUrl:'assets/tree.jpeg',
-      username:'nature',
-      content:'I saw this neat tree today'
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today'
     },
     {
       title: 'Snowy Mountain',
-      imageUrl:'assets/mountain.jpeg',
-      username:'mountainlover',
-      content:'Here is a picture of a snowy mountain'
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
     },
     {
       title: 'Mountain Biking',
-      imageUrl:'assets/biking.jpeg',
-      username:'biking12222',
-      content:'I did some biking today'
-    },
-    {
-      title: 'Rock Climbing',
-      imageUrl:'assets/biking.jpeg',
-      username:'biking12222',
-      content:'I did some biking today'
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biking12222',
+      content: 'I did some biking today'
     }
   ];
 }
